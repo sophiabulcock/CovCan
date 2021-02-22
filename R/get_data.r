7229780ddbd277162353358cc04b9144dbefa25a
Yuxuan Cui <cuiyuuxuan@gmail.com>
2021-02-22 12:28:30 -0800

    add

--- R/get_data.r
+++ R/get_data.r
@@ -26,7 +26,6 @@ Get_data<-function(apikey='4T9GEYHZ7PE9w8H29xynebW3L') {
     #' @import tidyverse
     #' @import ggrepel
     #' @import httr
-    #' @import dpylr
     #' @import tidyverse
     #' @param apikey API token from api.pify
     #' Defaults to "Tow8X4YNqnsWMFGbWxuPynzHh"
